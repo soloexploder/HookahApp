785e61e32716f26d413bb458be52d2c502c7daf5
Khanturov Daniil <soloexploder@gmail.com>
2021-04-19 17:36:48 +0300

    Initial Commit

---
+++ HookahApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HookahApp
+//
+//  Created by Даниил Хантуров on 19.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
