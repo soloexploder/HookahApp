25ada6abe77b7827668731c3f528f2616e237c73
Aleksandr Tegai <shura.tegay@mail.ru>
2021-04-21 13:09:08 +0300

    save changes

--- HookahApp/Model/Product.swift
+++ HookahApp/Model/Product.swift
@@ -6,6 +6,7 @@
 //
 
 
+
 struct Product {
     
     let name: String
@@ -15,7 +16,61 @@ struct Product {
     static func getProduct() -> [Product] {
         [
             Product(
-                name: "Крымский Вайб", tastes: ["Дыня, Арбуз, Персик"], tobacco: "Duft")
+                name: "Крымский Вайб",
+                tastes: ["дыня, арбуз, персик"],
+                tobacco: "Duft"
+            ),
+            
+            Product(
+                name: "Камчатский панч",
+                tastes: ["дыня, чай, черника"],
+                tobacco: "Duft"
+            ),
+            
+            Product(
+                name: "Донской чилл",
+                tastes: ["нуга, дыня, лимон"],
+                tobacco: "Duft"
+            ),
+            
+            Product(
+                name: "Приморский шейк",
+                tastes: ["черника, кокос, ананас"],
+                tobacco: "Duft"
+            )
         ]
+    }
+}
+
+enum Mix: String {
+        
+    case melon = "дыня"
+    case watermelon = "арбуз"
+    case peach = "персик"
+    case tea = "чай"
+    case blueberry = "черника"
+    case lemon = "лимон"
+    case coconut = "кокос"
+    case pineapple = "ананас"
+    
+    var mixer: String {
+        switch self {
+        case .melon:
+            <#code#>
+        case .watermelon:
+            <#code#>
+        case .peach:
+            <#code#>
+        case .tea:
+            <#code#>
+        case .blueberry:
+            <#code#>
+        case .lemon:
+            <#code#>
+        case .coconut:
+            <#code#>
+        case .pineapple:
+            <#code#>
+        }
     }
 }
