cb50df7264f6b924cac8390a98d79ce9be4b4359
Aleksandr Tegai <shura.tegay@mail.ru>
2021-04-21 11:48:49 +0300

    First push

---
+++ HookahApp/ViewControllers/TabBarViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TabBarViewController.swift
+//  HookahApp
+//
+//  Created by Alex Tegai on 21.04.2021.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    let products = Product.getProduct()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViewController(with: products)
+    }
+    
+    // MARK: - Navigation
+    
+    private func setupViewController(with products: [Product]) {
+        guard let viewControllers = tabBarController?.viewControllers else { return }
+        
+        viewControllers.forEach {
+            
+            if let menuVC = $0 as? MenuViewController {
+                menuVC.products = products
+            } else if let mixVC = $0 as? MixViewController {
+                mixVC.products = products
+            } else if let tobaccoVC = $0 as? TobaccoViewController {
+                tobaccoVC.products = products
+            }
+        }
+    }
+}
